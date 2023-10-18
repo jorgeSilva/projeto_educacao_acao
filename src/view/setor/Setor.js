986e9dfb92a391f2e11ab11c0ab10d061299a94d
jorgeSilva <jorgelfsilva5@gmail.com>
2023-10-18 15:55:26 -0300

    fix - correção no nome dado à exclusão dos dados cadastrados

--- src/view/setor/Setor.js
+++ src/view/setor/Setor.js
@@ -28,8 +28,6 @@ const Setor = () => {
   const [active, setActive] = React.useState(false)
   const [stop, setStop] = React.useState('')
 
-  console.log(type);
-
   return (
     <article className='container'>
       <section className='left__side__container'>
@@ -134,7 +132,7 @@ const Setor = () => {
                             active === 'update'?
                             <ModalUpdate content={{item, type:'convenio', setActive}}/>
                             :
-                            <ModalDelete content={{item, type:'convenio', setActive, h1: 'Exclua o registro do determinado convênio', name: 'Merenda'}}/>
+                            <ModalDelete content={{item, type:'convenio', setActive, h1: 'Exclua o registro do determinado convênio', name: 'merenda'}}/>
                           }
                         </>
                         :
@@ -194,51 +192,57 @@ const Setor = () => {
                   <>
                     <section className='rigth__side__card' key={item._id}>
                       {
-                        active && stop === item._id ? 
+                        active && stop === item._id ?
                         <>
-                        {
-                          active === 'update'?
-                          <ModalUpdate content={{item, type:'convenio', setActive}}/>
-                          :
-                          <ModalDelete content={{item, type:'convenio', setActive, h1: 'Exclua o registro do determinado funcionário', name: 'Transporte'}}/>
-                        }
-                      </>
-                      :
-                      <>
-                        {
-                          loading ?
-                          <span className="loader-"></span>
-                          :
-                          <>
-                            <div className='rigth__content__card__edit'>
-                              <p className='card__text'>
-                                <span>Nome:</span> {item.nome}
-                              </p>
-                              <div>
-                                <button className='card__button__edit' onClick={() => {
-                                  setActive('update')
-                                  setStop(item._id)
-                                }}>
-                                  <IconEdit/>
-                                </button>
-                                <button className='card__button__edit' onClick={() => {
-                                  setActive('delete')
-                                  setStop(item._id)
-                                }}>
-                                  <IconTrash/>
-                                </button>
+                          {
+                            active === 'update'?
+                            <ModalUpdate content={{item, type:'convenio', setActive}}/>
+                            :
+                            <ModalDelete content={{item, type:'convenio', setActive, h1: 'Exclua o registro do determinado convênio', name: 'transporte'}}/>
+                          }
+                        </>
+                        :
+                        <>
+                          {
+                            loading ?
+                            <span className="loader-"></span>
+                            :
+                            <>
+                              <div className='rigth__content__card__edit'>
+                                <p className='card__text'>
+                                  <span>Convênio:</span> {item.convenio}
+                                </p>
+                                <div>
+                                  <button className='card__button__edit' onClick={() => {
+                                    setActive('update')
+                                    setStop(item._id)
+                                  }}>
+                                    <IconEdit/>
+                                  </button>
+                                  <button className='card__button__edit' onClick={() => {
+                                    setActive('delete')
+                                    setStop(item._id)
+                                  }}>
+                                    <IconTrash/>
+                                  </button>
+                                </div>
                               </div>
-                            </div>
-                            <p className='card__text'>
-                              <span>Setor:</span> {item.setor}
-                            </p>
-                            <p className='card__text'>
-                              <span>Obs:</span> {item.obs}
-                            </p>
-                          </>
-                        }
-                      </>
-                    }
+                              <p className='card__text'>
+                                <span>PMI:</span> {item.pmi}
+                              </p>
+                              <p className='card__text'>
+                                <span>SEE:</span> {item.see}
+                              </p>
+                              <p className='card__text'>
+                                <span>Contrapartida:</span> {item.contraPartida}
+                              </p>
+                              <p className='card__text'>
+                                <span>Data:</span> {item.date}
+                              </p>
+                            </>
+                          }
+                        </>
+                      }
                     </section>
                   </>
                 )) 
